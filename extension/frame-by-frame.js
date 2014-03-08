e647d918d6163edcb5b1ddf52439cee8f60a4e61
Michael Yockney <Michael.yockney@gmail.com>
2014-03-09 04:29:06 +1300

    Added Scroll Support, Moved controls to Player, Styled Controls

--- extension/frame-by-frame.js
+++ extension/frame-by-frame.js
@@ -1,32 +1,80 @@
-fbf = {}
+fs = {};
 
-fbf.FRAMES_PER_SECOND = 25
-fbf.PLAYER_ID = "movie_player"
+fs.FRAMES_PER_SECOND = 25;
+fs.PLAYER_ID = "movie_player";
+fs.button = document.getElementById("frame-button")
+fs.frameScrollOn = false;
 
-fbf.prevFrame = function() {
+fs.prevFrame = function() {
     // Based on YouTube enhancer userscript, http://userscripts.org/scripts/show/33042.
-    player = document.getElementById(fbf.PLAYER_ID)
-    player.pauseVideo()
-    player.seekBy(-1 * (1/fbf.FRAMES_PER_SECOND))
+    player = document.getElementById(fs.PLAYER_ID);
+    player.pauseVideo();
+    player.seekBy(-1 * (1/fs.FRAMES_PER_SECOND));
 }
 
-fbf.nextFrame = function() {
+fs.nextFrame = function() {
     // Based on YouTube enhancer userscript, http://userscripts.org/scripts/show/33042.
-    player = document.getElementById(fbf.PLAYER_ID)
-    player.pauseVideo()
-    player.seekBy(1 * (1/fbf.FRAMES_PER_SECOND))
+    player = document.getElementById(fs.PLAYER_ID);
+    player.pauseVideo();
+    player.seekBy(1 * (1/fs.FRAMES_PER_SECOND));
 }
 
-fbf.injectControls = function() {
-    /*
-    Injects extra player controls into the page.
-    */
-    // Really basic controls to get started.
-    controls_html = "(<a href=\"javascript: fbf.prevFrame()\" style=\"color: red\">prev</a>"
-        + "/"
-        + "<a href=\"javascript: fbf.nextFrame()\" style=\"color: red\">next</a>)"
-    title_element = document.getElementById("watch-headline-title")
-    title_element.innerHTML = controls_html + title_element.innerHTML
+
+//Scroll Frames
+fs.frameScroll = function() {
+	if(fs.frameScrollOn){
+		fs.frameScrollOn = false;
+		document.getElementsByTagName('body')[0].className = document.getElementsByTagName('body')[0].className.replace( /(?:^|\s)no-scroll(?!\S)/g , '' );
+	}else if(!fs.frameScrollOn){
+		fs.frameScrollOn = true;
+		document.getElementsByTagName('body')[0].className += ' no-scroll';
+	}
+}		
+
+fs.scrollFunc = function(e){
+        var direct=0;
+    e=e || window.event;
+
+    if(e.wheelDelta && fs.frameScrollOn){
+        fs.userMouse(e.wheelDelta);
+    }else if(e.detail){
+        fs.userMouse(e.wheelDelta);
+    }
+    }
+    if(document.addEventListener && fs.frameScrollOn){
+    document.addEventListener('DOMMouseScroll',fs.scrollFunc,false);
 }
 
-fbf.injectControls()
+document.onmousewheel = fs.scrollFunc;
+
+fs.userMouse = function(flage){
+    if(flage == 3){
+        fs.nextFrame();
+    }else if(flage == -3){
+        fs.prevFrame();
+	}else if(flage == 120){
+        fs.nextFrame();
+    }else if(flage == -120){
+        fs.prevFrame();
+    }
+}	
+
+
+// Inject Controls
+fs.injectControls = function() {
+    var controls = document.createElement('controls_html');
+    controls.innerHTML = "<div class='ytb-button' style=\"float:right; color: #939393;\"><a href=\"javascript: fs.prevFrame()\" style=\"color: #939393; margin-left:5px; text-decoration:none;\">< </a>"
+        + "<img id='frame-button' src='http://www.iconsdb.com/icons/preview/gray/film-xxl.png' style='width:15px; height:12.5px; margin-top:7px; cursor:pointer;'>" /*temporary src link*/
+        + "<a href=\"javascript: fs.nextFrame()\" style=\"color: #939393; margin-right:5px; text-decoration:none;\"> ></a></div>"
+    var html5player = document.getElementsByClassName("html5-player-chrome")[0];
+    html5player.appendChild(controls);
+    document.getElementById("frame-button").addEventListener( 'click' , fs.frameScroll );
+    
+    //Create no-scroll class
+    var style = document.createElement('style');
+	style.type = 'text/css';
+	style.innerHTML = '.no-scroll {height: 100%; overflow: hidden;}';
+	document.getElementsByTagName('head')[0].appendChild(style);
+}
+
+fs.injectControls();
